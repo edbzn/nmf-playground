a18d122db0087e681dce36162517da975a233feb
Edouard Bozon <bozonedouard@gmail.com>
2024-02-18 09:25:45 +0100

    feat: add import map overrides

--- apps/host/federation.config.js
+++ apps/host/federation.config.js
@@ -1,13 +1,17 @@
-const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+const {
+  withNativeFederation,
+  shareAll,
+} = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      eager: true,
+      pinned: true,
+      strictVersion: true,
+      requiredVersion: 'auto',
+    }),
   },
-  skip: [
-    'rxjs/ajax',
-    'rxjs/fetch',
-    'rxjs/testing',
-    'rxjs/webSocket',
-  ]
+  skip: ['rxjs/ajax', 'rxjs/fetch', 'rxjs/testing', 'rxjs/webSocket'],
 });
